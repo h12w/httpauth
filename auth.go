162373796906105353071789947eef1a1a03f650
Hǎiliàng Wáng <hwang.dev@gmail.com>
2015-03-06 02:01:10 +0800

    simplify

--- auth.go
+++ auth.go
@@ -43,10 +43,10 @@ type Role int
 // users, you should not specify a hash; it will be generated in the Register
 // and Update functions.
 type UserData struct {
-	Username string `bson:"Username"`
-	Email    string `bson:"Email"`
-	Hash     []byte `bson:"Hash"`
-	Role     string `bson:"Role"`
+	Name  string `bson:"Name"`
+	Email string `bson:"Email"`
+	Hash  []byte `bson:"Hash"`
+	Role  string `bson:"Role"`
 }
 
 // Authorizer structures contain the store of user session cookies a reference
@@ -73,15 +73,6 @@ func (a Authorizer) addMessage(rw http.ResponseWriter, req *http.Request, messag
 	messageSession, _ := a.cookiejar.Get(req, "messages")
 	defer messageSession.Save(req, rw)
 	messageSession.AddFlash(message)
-}
-
-// Helper function to save a redirect to the page a user tried to visit before
-// logging in.
-func (a Authorizer) goBack(rw http.ResponseWriter, req *http.Request) {
-	redirectSession, _ := a.cookiejar.Get(req, "redirects")
-	defer redirectSession.Save(req, rw)
-	redirectSession.Flashes()
-	redirectSession.AddFlash(req.URL.Path)
 }
 
 func mkerror(msg string) error {
@@ -148,11 +139,8 @@ func (a Authorizer) Login(rw http.ResponseWriter, req *http.Request, u string, p
 // Pass in a instance of UserData with at least a username and email specified. If no role
 // is given, the default one is used.
 func (a Authorizer) Register(rw http.ResponseWriter, req *http.Request, user UserData, password string) error {
-	if user.Username == "" {
+	if user.Name == "" {
 		return mkerror("no username given")
-	}
-	if user.Email == "" {
-		return mkerror("no email given")
 	}
 	if user.Hash != nil {
 		return mkerror("hash will be overwritten")
@@ -162,9 +150,9 @@ func (a Authorizer) Register(rw http.ResponseWriter, req *http.Request, user Use
 	}
 
 	// Validate username
-	_, err := a.backend.User(user.Username)
+	_, err := a.backend.User(user.Name)
 	if err == nil {
-		a.addMessage(rw, req, "Username has been taken.")
+		a.addMessage(rw, req, "Name has been taken.")
 		return mkerror("user already exists")
 	} else if err != ErrMissingUser {
 		if err != nil {
@@ -174,7 +162,7 @@ func (a Authorizer) Register(rw http.ResponseWriter, req *http.Request, user Use
 	}
 
 	// Generate and save hash
-	hash, err := bcrypt.GenerateFromPassword([]byte(user.Username+password), 8)
+	hash, err := bcrypt.GenerateFromPassword([]byte(user.Name+password), 8)
 	if err != nil {
 		return mkerror("couldn't save password: " + err.Error())
 	}
@@ -203,8 +191,8 @@ func (a Authorizer) Update(rw http.ResponseWriter, req *http.Request, p string, 
 		hash  []byte
 		email string
 	)
-	authSession, err := a.cookiejar.Get(req, "auth")
-	username, ok := authSession.Values["username"].(string)
+	session, err := a.cookiejar.Get(req, "auth")
+	username, ok := session.Values["username"].(string)
 	if !ok {
 		return mkerror("not logged in")
 	}
@@ -238,86 +226,49 @@ func (a Authorizer) Update(rw http.ResponseWriter, req *http.Request, p string, 
 	return nil
 }
 
+// ChangeRole changes the role for an existing user.
+func (a Authorizer) ChangeRole(user *UserData, role string) error {
+	var u UserData = *user
+	u.Role = role
+	return a.backend.SaveUser(u)
+}
+
 // Authorize checks if a user is logged in and returns an error on failed
 // authentication. If redirectWithMessage is set, the page being authorized
 // will be saved and a "Login to do that." message will be saved to the
 // messages list. The next time the user logs in, they will be redirected back
 // to the saved page.
-func (a Authorizer) Authorize(rw http.ResponseWriter, req *http.Request, redirectWithMessage bool) error {
-	authSession, err := a.cookiejar.Get(req, "auth")
+func (a Authorizer) Authorize(rw http.ResponseWriter, req *http.Request) (*UserData, error) {
+	session, err := a.cookiejar.Get(req, "auth")
 	if err != nil {
-		if redirectWithMessage {
-			a.goBack(rw, req)
-		}
-		return mkerror("new authorization session")
+		return nil, mkerror("new authorization session")
 	}
-	/*if authSession.IsNew {
-	    if redirectWithMessage {
-	        a.goBack(rw, req)
-	        a.addMessage(rw, req, "Log in to do that.")
-	    }
-	    return mkerror("no session existed")
-	}*/
-	username := authSession.Values["username"]
-	if !authSession.IsNew && username != nil {
-		_, err := a.backend.User(username.(string))
-		if err == ErrMissingUser {
-			authSession.Options.MaxAge = -1 // kill the cookie
-			authSession.Save(req, rw)
-			if redirectWithMessage {
-				a.goBack(rw, req)
-				a.addMessage(rw, req, "Log in to do that.")
-			}
-			return mkerror("user not found")
-		} else if err != nil {
-			return mkerror(err.Error())
-		}
-	}
+	username := session.Values["username"]
 	if username == nil {
-		if redirectWithMessage {
-			a.goBack(rw, req)
-			a.addMessage(rw, req, "Log in to do that.")
-		}
-		return mkerror("user not logged in")
+		return nil, mkerror("user not logged in")
 	}
-	return nil
+	if name, ok := username.(string); ok {
+		user, err := a.backend.User(name)
+		if err == ErrMissingUser {
+			session.Options.MaxAge = -1 // kill the cookie
+			session.Save(req, rw)
+			return nil, mkerror("user not found")
+		} else if err != nil {
+			return nil, mkerror(err.Error())
+		}
+		return &user, nil
+	}
+	return nil, mkerror("user not found")
 }
 
 // AuthorizeRole runs Authorize on a user, then makes sure their role is at
 // least as high as the specified one, failing if not.
-func (a Authorizer) AuthorizeRole(rw http.ResponseWriter, req *http.Request, role string, redirectWithMessage bool) error {
+func (a Authorizer) Satisfy(user *UserData, role string) bool {
 	r, ok := a.roles[role]
 	if !ok {
-		return mkerror("role not found")
+		return false
 	}
-	if err := a.Authorize(rw, req, redirectWithMessage); err != nil {
-		return mkerror(err.Error())
-	}
-	authSession, _ := a.cookiejar.Get(req, "auth") // should I check err? I've already checked in call to Authorize
-	username := authSession.Values["username"]
-	if user, err := a.backend.User(username.(string)); err == nil {
-		if a.roles[user.Role] >= r {
-			return nil
-		}
-		a.addMessage(rw, req, "You don't have sufficient privileges.")
-		return mkerror("user doesn't have high enough role")
-	}
-	return mkerror("user not found")
-}
-
-// CurrentUser returns the currently logged in user and a boolean validating
-// the information.
-func (a Authorizer) CurrentUser(rw http.ResponseWriter, req *http.Request) (user UserData, e error) {
-	if err := a.Authorize(rw, req, false); err != nil {
-		return user, mkerror(err.Error())
-	}
-	authSession, _ := a.cookiejar.Get(req, "auth")
-
-	username, ok := authSession.Values["username"].(string)
-	if !ok {
-		return user, mkerror("User not found in authsession")
-	}
-	return a.backend.User(username)
+	return a.roles[user.Role] >= r
 }
 
 // Logout clears an authentication session and add a logged out message.
